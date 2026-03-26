179d23c7a8b37a9e0080fc3f4fc0309f30e90e9a
josuechacha31-cloud <josuechacha31@gmail.com>
2026-03-26 12:23:22 -0500

    Add files via upload
    creacion proyecto

---
+++ app.js
@@ -0,0 +1,26 @@
+const SB_URL = "https://vmorgejoxarkypgeavin.supabase.co";
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtb3JnZWpveGFya3lwZ2VhdmluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NDAxODAsImV4cCI6MjA5MDExNjE4MH0.Snj2a7UVGvYhXfE8_1Rx-X91fupnPq-4A9fVMAj38jQ";
+const supabaseClient = supabase.createClient(SB_URL, SB_KEY);
+
+async function login() {
+    const email = document.getElementById('email-input').value;
+
+    const { data, error } = await supabaseClient
+        .from('personas')
+        .select('nombre, roles(nombre_rol)')
+        .eq('email', email)
+        .single();
+
+    if (error || !data) {
+        alert("Usuario no registrado en la base de datos.");
+        return;
+    }
+
+    document.getElementById('login-box').classList.add('hidden');
+    
+    if (data.roles.nombre_rol === 'Administrador') {
+        document.getElementById('dash-admin').classList.remove('hidden');
+    } else {
+        document.getElementById('dash-empleado').classList.remove('hidden');
+    }
+}
